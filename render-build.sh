119a24b7eb2ad5905bd6c73b49870c2a88e5d78e
macair gold <macamacairgoldirgold@MacBook-Air-khxng-macair.local>
2025-09-25 20:35:11 +0700

    Fix render-build.sh for Render build

--- render-build.sh
+++ render-build.sh
@@ -1,22 +1,6 @@
 #!/usr/bin/env bash
 set -o errexit
 
-# ติดตั้ง dependencies ที่ canvas ต้องใช้
-if [ -f Aptfile ]; then
-  apt-get update
-  apt-get install -y --no-install-recommends $(cat Aptfile)
-fi
-
 # ติดตั้ง Node modules
-npm install
-#!/usr/bin/env bash
-set -e
-
-# ติดตั้ง library ที่จำเป็นสำหรับ node-canvas
-apt-get update
-DEBIAN_FRONTEND=noninteractive apt-get install -y \
-  libcairo2-dev libpango1.0-dev libjpeg-dev libgif-dev librsvg2-dev
-
-# ติดตั้ง dependencies ของโปรเจกต์
 npm install
 
